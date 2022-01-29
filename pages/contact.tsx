cb69efa5c4997c6b3c887fd94fb16afa4ea1823b
manfred <manfredmiravitllasmas@gmail.com>
2022-01-29 22:34:23 +0100

    Font awesome icons added for contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,6 +2,9 @@ import type { NextPage } from 'next'
 import Layout from 'components/layout'
 import Navigation from 'components/navigation'
 import useTranslation from 'next-translate/useTranslation'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithubAlt, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import styles from 'components/contact.module.css';
 
 const Contact: NextPage = () => {
   const { t } = useTranslation();
@@ -17,11 +20,28 @@ const Contact: NextPage = () => {
           <p className="text-center mb-10">
             <a href="">{t('contact:cv')}</a>
           </p>
-          <ul>
-            <li>
-              <a href="https://www.linkedin.com/in/manfredmmm" rel="noreferrer" target="_blank"><i className="fa fa-linkedin"></i></a>
-            </li>
-          </ul>
+          <div className="flex flex-row text-center space-x-4">
+            <div className="w-1/4 text-center">
+              <a className="bg-gray-dark rounded-full block" href="https://www.linkedin.com/in/manfredmmm" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faLinkedinIn} className={styles.social} />
+              </a>
+            </div>
+            <div className="w-1/4 text-center">
+              <a className="bg-gray-dark rounded-full block" href="https://github.com/manfredmmm" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faGithubAlt} className={styles.social} />
+              </a>
+            </div>
+            <div className="w-1/4 text-center">
+              <a className="bg-gray-dark rounded-full block" href="https://twitter.com/manfredmmm" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faTwitter} className={styles.social} />
+              </a>
+            </div>
+            <div className="w-1/4 text-center">
+              <a className="bg-gray-dark rounded-full block" href="https://www.instagram.com/manfredmmm/" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faInstagram} className={styles.social} />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <Navigation prev="/can" next="" textColor="white" />
