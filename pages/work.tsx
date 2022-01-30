8f6c32236aa6cfddd0aa54e196d8af4ef87b2a6f
manfred <manfredmiravitllasmas@gmail.com>
2022-01-30 19:06:54 +0100

    Work page

--- pages/work.tsx
+++ pages/work.tsx
@@ -8,8 +8,24 @@ const Work: NextPage = () => {
   return (
     <Layout title={`manfredmmm - ${t('common:work').toLowerCase()}`}>
       <div className="bg-white-darkest h-full flex justify-center items-center">
-        <div>
-          <h1>{t('work:title')}</h1>
+        <div className="max-w-xl">
+          <h1 className="text-xl uppercase font-heading">{t('work:title')}</h1>
+          <p className="text-xs uppercase mb-4 mt-8">{t('work:current')}</p>
+          <h4 className="mb-1">{t('work:ifs')}</h4>
+          <p className="text-xs mb-4">{t('work:ifs_description')}</p>
+          <p className="text-xs uppercase mb-4 mt-8">{t('work:previous')}</p>
+          <h4 className="mb-1">{t('work:beezy')}</h4>
+          <p className="text-xs mb-4">{t('work:beezy_description')}</p>
+          <h4 className="mb-1">{t('work:adverway')}</h4>
+          <p className="text-xs mb-4">{t('work:adverway_description')}</p>
+          <h4 className="mb-1">{t('work:imesmes')}</h4>
+          <p className="text-xs mb-4">
+            <span>{t('work:imesmes_description')}</span>
+            <br />
+            <span>{t('work:imesmes_description_2')}</span>
+          </p>
+          <h4 className="mb-1">{t('work:i3dat')}</h4>
+          <p className="text-xs mb-4">{t('work:i3dat_description')}</p>
         </div>
       </div>
       <Navigation prev="/about" next="/can" />
