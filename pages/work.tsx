5ca8b86c59129c04d0f1021201661856e9cf5fb9
manfred <manfredmiravitllasmas@gmail.com>
2023-09-06 18:35:45 +0200

    Work copies page updates

--- pages/work.tsx
+++ pages/work.tsx
@@ -13,7 +13,7 @@ const Work: NextPage = () => {
           <h1 className="text-xl uppercase font-heading mb-4">{t('work:title')}</h1>
           <span className="text-xs uppercase">{t('work:current')}</span>
           <h4 className="mb-1">{t('work:factorial')}</h4>
-          <p className="mb-4 text-xs">
+          <p className="text-xs mb-4">
             <span className="block text-sm mb-1">{t('work:factorial_description')}</span>
             <span>{t('work:factorial_stack')}</span>
           </p>
@@ -39,11 +39,7 @@ const Work: NextPage = () => {
           <h4 className="mb-1">{t('work:imesmes')}</h4>
           <p className="text-xs mb-4">
             <span className="block text-sm mb-1">{t('work:imesmes_description')}</span>
-            <span>{t('work:imesmes_stack')}</span></p>
-          <h4 className="mb-1">{t('work:i3dat')}</h4>
-          <p className="text-xs mb-4">
-            <span className="block text-sm mb-1">{t('work:i3dat_description')}</span>
-            <span>{t('work:i3dat_stack')}</span>
+            <span>{t('work:imesmes_stack')}</span>
           </p>
         </div>
       </div>
